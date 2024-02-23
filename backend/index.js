9039bb92f173271b74bc5067db34e55f278b0ac5
devtanveer <158012528+devtanveer@users.noreply.github.com>
2024-02-23 18:14:10 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const corsOptions = {
-  origin: 'https://frontend-delta-six-53.vercel.app', 
+  origin: '*', 
   credentials: true,
   methods: ["POST","GET","DELETE"],
   optionSuccessStatus: 200,
